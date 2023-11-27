26af941d4ec344fdc57d3813f9e392994b0550b9
whysofail <adam.satria.armansyah@gmail.com>
2023-11-27 17:46:40 +0700

    add : seeders

---
+++ seeders/20231126022233-roles.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+    */
+   await queryInterface.bulkInsert('Roles',[
+    {
+      rolename: 'user'
+    },
+    {
+      rolename: 'admin'
+    },
+    {
+      rolename: 'lawyer'
+    },
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
