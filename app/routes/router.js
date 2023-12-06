0ecf9a6636d8483f57bb4d6669a07fb8d6e5d97c
whysofail <adam.satria.armansyah@gmail.com>
2023-12-07 00:36:36 +0700

    fix : route

--- app/routes/router.js
+++ app/routes/router.js
@@ -15,7 +15,7 @@ router.get("/who", middleware.authorize, authController.whoAmI);
 router.get('/lawyer', lawyerController.getLawyer);
 router.get('/lawyer/search', lawyerController.searchLawyerByTag);
 
-router.get('/chatstatus', middleware.getIdTokenFromMetadataServer, chatController.getServerStatus)
+router.get('/botstatus', middleware.getIdTokenFromMetadataServer, chatController.getServerStatus)
 router.post('/chat', middleware.authorize, middleware.getIdTokenFromMetadataServer, chatController.getChat)
 
 
