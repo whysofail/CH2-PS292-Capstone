10b6e858e13ed034a8dda664ab65e20511da7770
whysofail <adam.satria.armansyah@gmail.com>
2023-12-07 02:13:13 +0700

    fix

--- app/controllers/chatController.js
+++ app/controllers/chatController.js
@@ -2,29 +2,24 @@ const axios = require("axios");
 
 const { ML_URI } = process.env;
 
-const options = (path, method, bodyData, idToken) => {
-  const requestOptions = {
-    url: `${ML_URI}/`+path,
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      "X-Serverless-Authorization": `Bearer ${idToken}`,
-    },
-    data: bodyData,
-  };
-
-  return requestOptions;
-};
-
 const getServerStatus = async (req, res) => {
   const idToken = req.idToken;
-  console.log(idToken)
   try {
-    const response = await axios(options("", "GET", "", idToken));
-    return res.status(200).json(response.data);
+    const response = await axios.get(`${ML_URI}/`, {
+      headers: {
+        'X-Serverless-Authorization' : `Bearer ${idToken}`
+      }
+    });
+    if (response.status === 200) {
+      return res.status(200).json({ status: "OK", data: response.data });
+    } else {
+      return res
+        .status(response.status)
+        .json({ status: "Error", message: "Unexpected status code" });
+    }
   } catch (error) {
-    console.error("Error getting server status:", error);
-    return res.status(500).json(error);
+    console.error("Error making HTTP request:", error.message);
+    return res.status(500).json({ status: "Error", message: "Internal Server Error" });
   }
 };
 
@@ -35,7 +30,9 @@ const getChat = async (req, res) => {
   console.log(`Id Token: ${idToken}`);
 
   try {
-    const response = await axios(options("chat", "POST", { user_input }, idToken));
+    const response = await axios(
+      options("chat", "POST", { user_input }, idToken)
+    );
     console.log(response.data);
     return res.status(200).json(response.data);
   } catch (error) {
