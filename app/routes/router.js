2c125b87c21764f1c63a074c19a013e644a12272
whysofail <adam.satria.armansyah@gmail.com>
2023-12-07 00:38:54 +0700

    fix : router

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-const { authController, lawyerController, databaseController, chatController } = require("../controllers");
+const {
+  authController,
+  lawyerController,
+  databaseController,
+  chatController,
+} = require("../controllers");
 
 const middleware = require("../middleware");
 
-router.get('/databasestatus', databaseController.checkDatabaseConn)
+router.get("/databasestatus", databaseController.checkDatabaseConn);
 
-router.post("/login", authController.login)
-router.get('/logout', authController.logout)
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 router.post("/register", middleware.emailExist, authController.register);
 router.get("/who", middleware.authorize, authController.whoAmI);
 
-router.get('/lawyer', lawyerController.getLawyer);
-router.get('/lawyer/search', lawyerController.searchLawyerByTag);
+router.get("/lawyer", lawyerController.getLawyer);
+router.get("/lawyer/search", lawyerController.searchLawyerByTag);
 
-router.get('/botstatus', middleware.getIdTokenFromMetadataServer, chatController.getServerStatus)
-router.post('/chat', middleware.authorize, middleware.getIdTokenFromMetadataServer, chatController.getChat)
-
+router.get(
+  "/bots",
+  middleware.getIdTokenFromMetadataServer,
+  chatController.getServerStatus
+);
+router.post(
+  "/chat",
+  middleware.authorize,
+  middleware.getIdTokenFromMetadataServer,
+  chatController.getChat
+);
 
 module.exports = router;
