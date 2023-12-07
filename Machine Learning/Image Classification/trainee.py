a7f1d04f28273b9b5b611f5ab83a168d409f94ab
ichiyon01 <shandiadh1401@gmail.com>
2023-12-07 19:36:16 +0700

    change format images dataset and make model Image classification(up to done:-save model)

---
+++ Machine Learning/Image Classification/trainee.py
@@ -0,0 +1,138 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import random
+import os
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.keras.optimizers import RMSprop
+
+#load data
+base_dir = '../Dataset/Images'
+print('Content of base directory : ')
+print(os.listdir(base_dir))
+
+print("\nContents of train directory:")
+print(os.listdir(f'{base_dir}/train'))
+
+print("\nContents of validation directory:")
+print(os.listdir(f'{base_dir}/validation'))
+
+train_dir = os.path.join(base_dir, 'train')
+test_dir = os.path.join(base_dir, 'validation')
+
+train_chat_dir = os.path.join(train_dir, 'chat')
+train_image_dir = os.path.join(train_dir, 'image')
+
+test_chat_dir = os.path.join(test_dir, 'chat')
+test_image_dir = os.path.join(test_dir, 'image')
+
+train_chat_fnames = os.listdir(train_chat_dir)
+train_image_fnames = os.listdir(train_image_dir)
+
+print(train_chat_fnames[:10])
+print(train_image_fnames[:10])
+
+print('total training chat: ', len(os.listdir(train_chat_dir)))
+print('total training image: ', len(os.listdir(train_image_dir)))
+
+print('total validation chat: ', len(os.listdir(test_chat_dir)))
+print('total validation image: ', len(os.listdir(test_image_dir)))
+
+
+nrows = 4
+ncols = 4
+
+pic_index = 0
+
+fig = plt.gcf()
+fig.set_size_inches(ncols*4, nrows*4)
+
+pic_index+=8
+
+next_chat_pix = [os.path.join(train_chat_dir, fname)
+                 for fname in train_chat_fnames[pic_index-8:pic_index]
+                 ]
+
+next_image_pix = [os.path.join(train_image_dir, fname)
+                  for fname in train_image_fnames[pic_index-8:pic_index]
+                  ]
+
+for i, img_path in enumerate(next_chat_pix+next_image_pix):
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('off')
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+model = tf.keras.models.Sequential([
+  # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2), 
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'), 
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(), 
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'), 
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    tf.keras.layers.Dense(1, activation='sigmoid') 
+])
+
+model.summary()
+
+model.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+              metrics = ['accuracy'])
+
+# All images will be rescaled by 1./255.
+train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
+
+# --------------------
+# Flow training images in batches of 20 using train_datagen generator
+# --------------------
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))     
+# --------------------
+# Flow validation images in batches of 20 using test_datagen generator
+# --------------------
+validation_generator =  test_datagen.flow_from_directory(test_dir,
+                                                         batch_size=20,
+                                                         class_mode  = 'binary',
+                                                         target_size = (150, 150))
+
+history = model.fit(
+            train_generator,
+            epochs=15,
+            validation_data=validation_generator,
+            verbose=2
+            )
+
+# Function to preprocess the uploaded image
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(150, 150))  # Adjust target_size as needed
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)  # Add an extra dimension for batch size
+    img_array /= 255.0  # Rescale to the range [0, 1]
+    return img_array
+
+# Example usage
+uploaded_image_path = r"C:\Users\LENOVO\Desktop\CH2-PS292-Capstone\Machine Learning\Dataset\Images\Screenshot 2023-12-07 020954.png"
+preprocessed_image = preprocess_image(uploaded_image_path)
+
+# Make a prediction using the trained model
+prediction = model.predict(preprocessed_image)
+
+# Display the prediction
+if prediction[0] > 0.5:
+    print("The model predicts the image is in the 'image' class.")
+else:
+    print("The model predicts the image is in the 'chat' class.")
---
+++ Machine Learning/NLP Model/modelV2.py
@@ -0,0 +1,123 @@
+# import json
+# import tensorflow as tf
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from sklearn.model_selection import train_test_split
+# import numpy as np
+
+# file_path = '../Dataset/Dataset.json'
+# # Your dataset
+# with open(file_path, 'r', encoding='utf-8') as file:
+#     dataset = json.load(file)
+
+# # Prepare data
+# texts = []
+# labels = []
+# for intent in dataset["intents"]:
+#     for pattern in intent["patterns"]:
+#         texts.append(pattern)
+#         labels.append(intent["tag"])
+
+# # Tokenize the text
+# tokenizer = Tokenizer(oov_token="<OOV>")
+# tokenizer.fit_on_texts(texts)
+# word_index = tokenizer.word_index
+
+# # Convert text to sequences
+# sequences = tokenizer.texts_to_sequences(texts)
+# padded_sequences = pad_sequences(sequences, padding="post")
+
+# # Convert labels to one-hot encoding
+# label_set = list(set(labels))
+# label_dict = {label: i for i, label in enumerate(label_set)}
+# labels = [label_dict[label] for label in labels]
+# one_hot_labels = tf.keras.utils.to_categorical(labels, num_classes=len(label_set))
+
+# # Split the data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(padded_sequences, one_hot_labels, test_size=0.2, random_state=42)
+
+# # Build the model
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Embedding(len(word_index) + 1, 32, input_length=padded_sequences.shape[1]),
+#     tf.keras.layers.GlobalAveragePooling1D(),
+#     tf.keras.layers.Dense(256, activation="relu"),
+#     tf.keras.layers.Dense(128, activation="relu"),
+#     tf.keras.layers.Dense(64, activation="relu"),
+#     tf.keras.layers.Dense(len(label_set), activation="softmax")
+# ])
+
+# model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# # Train the model
+# model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+
+# # Example usage
+# user_input = "saya mengalami pelecehan seksual"
+# sequence = pad_sequences(tokenizer.texts_to_sequences([user_input]), padding="post", maxlen=padded_sequences.shape[1])
+# prediction = model.predict(sequence)
+# predicted_label = label_set[np.argmax(prediction)]
+# response = [intent["responses"] for intent in dataset["intents"] if intent["tag"] == predicted_label][0][0]
+
+# print(response)
+import json
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+file_path = '../Dataset/Dataset.json'
+# Load your dataset from a JSON file
+with open(file_path, 'r', encoding='utf-8') as file:
+    dataset = json.load(file)
+
+# Prepare data
+texts = []
+labels = []
+for intent in dataset["intents"]:
+    for pattern in intent["patterns"]:
+        texts.append(pattern)
+        labels.append(intent["tag"])
+
+# Tokenize the text
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(texts)
+word_index = tokenizer.word_index
+
+# Convert text to sequences
+sequences = tokenizer.texts_to_sequences(texts)
+padded_sequences = pad_sequences(sequences, padding="post")
+
+# Convert labels to one-hot encoding
+label_set = list(set(labels))
+label_dict = {label: i for i, label in enumerate(label_set)}
+labels = [label_dict[label] for label in labels]
+one_hot_labels = tf.keras.utils.to_categorical(labels, num_classes=len(label_set))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(padded_sequences, one_hot_labels, test_size=0.2, random_state=42)
+
+# Build the model with LSTM layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(len(word_index) + 1, 64, input_length=padded_sequences.shape[1]),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128)),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(label_set), activation="softmax")
+])
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# Train the model
+model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+
+# Example usage
+user_input = "Hi there, how can you help me?"
+sequence = pad_sequences(tokenizer.texts_to_sequences([user_input]), padding="post", maxlen=padded_sequences.shape[1])
+prediction = model.predict(sequence)
+predicted_label = label_set[np.argmax(prediction)]
+response = [intent["responses"] for intent in dataset["intents"] if intent["tag"] == predicted_label][0][0]
+
+print(response)
