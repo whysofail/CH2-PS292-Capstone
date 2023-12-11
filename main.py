3653c2ae1779a7cfa690c688906527bb9c8cb31f
whysofail <adam.satria.armansyah@gmail.com>
2023-12-12 00:13:58 +0700

    test

--- main.py
+++ main.py
@@ -81,5 +81,4 @@ def get_response(intents_list, intents_json):
             break
     return result
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
