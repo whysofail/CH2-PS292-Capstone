f253d9723a074a58b9eed8cc59d48fc9915c75a7
ichiyon01 <shandiadh1401@gmail.com>
2023-12-13 16:11:28 +0700

    add image classification in main.py and some requirements in requirements.txt

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ import pickle
 import json
 import nltk
 import os
+import random
+import requests
+
+from io import BytesIO
+from PIL import Image
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 from flask import Flask, request, jsonify
+from tensorflow.keras.preprocessing.image import img_to_array
 
 app = Flask(__name__)
 
@@ -19,10 +25,13 @@ file_path = os.path.join('.','app','dataset','dataset.json')
 words_path = os.path.join('.','app','models', 'words.pkl')
 classes_path = os.path.join('.','app','models', 'classes.pkl')
 model_path =  os.path.join('.','app','models', 'model_chatbot.h5')
+model_image_path = os.path.join('.', 'app', 'models', 'model_image.h5')
 
 words = pickle.load(open(words_path, 'rb'))
 classes = pickle.load(open(classes_path , 'rb'))
 model = load_model(model_path)
+model_image = load_model(model_image_path)
+
 with open(file_path, 'r', encoding='utf-8') as file:
   intents_json = json.load(file)
 
@@ -41,7 +50,20 @@ def predict():
 
     return jsonify({"msg": response,
                    "tag": tag})
-  # nambah tags
+
+@app.route('/images', methods=['POST'])
+def image_predict():
+    data = request.get_json()
+    image_url = data['image_url']
+
+    preprocessed_image = preprocess_image_from_url(image_url)
+    prediction = model_image.predict(preprocessed_image)
+
+    
+    if prediction[0] > 0.5:
+        return jsonify({"msg": "image"})
+    else:
+        return jsonify({"msg": "chat"})
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
@@ -74,16 +96,22 @@ def get_response(intents_list, intents_json):
         if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
-    return result
-
-def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
     return tag, result
+
+#image processing
+def preprocess_image_from_url(image_url):
+    try:
+        response = requests.get(image_url)
+        response.raise_for_status()  # Check for HTTP errors
+        img = Image.open(BytesIO(response.content))
+        img = img.resize((150, 150))  # Adjust the size as needed
+        img_array = img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)  # Add an extra dimension for batch size
+        img_array /= 255.0  # Rescale to the range [0, 1]
+        return img_array
+    except Exception as e:
+        print(f"Error processing image from URL: {e}")
+        return None
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
