371c6774630978ad19eb8d5e6809b47f4c818a3d
whysofail <adam.satria.armansyah@gmail.com>
2023-12-14 08:51:01 +0700

    route separation

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,37 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const consultationRoute = require('./consultationRoute')
+const consultationRoute = require("./consultationRoute");
+const authRoute = require("./authRoute");
+const lawyerRoute = require('./lawyerRoute')
+const statusRoute = require('./statusRoute')
 const {
-  authController,
-  lawyerController,
-  databaseController,
   chatController,
-  imageController,
-  consultationController,
 } = require("../controllers");
 
 const middleware = require("../middleware");
-
-router.get(
-  "/chat",
-  middleware.iam.getIdTokenFromMetadataServer,
-  chatController.getServerStatus
-);
-
-router.get("/databasestatus", databaseController.checkDatabaseConn);
-
-router.post("/login", authController.login);
-router.get("/logout", authController.logout);
-router.post(
-  "/register",
-  middleware.emailCheck.emailExist,
-  authController.register
-);
-router.get("/who", middleware.authorization.authorize, authController.whoAmI);
-
-router.get("/lawyer", lawyerController.getLawyer);
-router.get("/lawyer/search", lawyerController.searchLawyerByTag);
 
 router.post(
   "/chat",
@@ -40,12 +18,9 @@ router.post(
   chatController.getChat
 );
 
-router.post(
-  "/testimage",
-  middleware.uploadFileGCS.uploadImage,
-  imageController.catchImageURI
-);
-
-router.use('/consultation', consultationRoute)
+router.use("/auth", authRoute);
+router.use('/lawyer', lawyerRoute)
+router.use("/consultation", consultationRoute);
+router.use('/status', statusRoute)
 
 module.exports = router;
