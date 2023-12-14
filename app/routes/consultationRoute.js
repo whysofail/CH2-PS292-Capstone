196f53820a113c4fbf454a7d59eb0a67afb91857
whysofail <adam.satria.armansyah@gmail.com>
2023-12-14 09:06:21 +0700

    modified : consultation

--- app/routes/consultationRoute.js
+++ app/routes/consultationRoute.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  consultationController,
-} = require("../controllers");
+const { consultationController } = require("../controllers");
 
 const middleware = require("../middleware");
 
-
 router.get(
-    "/",
-    middleware.authorization.authorize,
-    consultationController.getAllConsultation
-  );
-  router.get(
-    "/:id",
-    middleware.authorization.authorize,
-    consultationController.getConsultationById
-  );
-  router.post(
-    "/:id",
-    middleware.authorization.authorize,
-    middleware.uploadFileGCS.uploadImage,
-    consultationController.createConsultation
-  );
-  router.put(
-    "/:id",
-    middleware.authorization.authorize,
-    middleware.uploadFileGCS.uploadImage,
-    consultationController.updateConsultation
-  );
+  "/",
+  middleware.authorization.authorize,
+  consultationController.getAllConsultation
+);
+router.get(
+  "/user",
+  middleware.authorization.authorize,
+  consultationController.getAllConsultationByUserId
+);
+router.post(
+  "/",
+  middleware.authorization.authorize,
+  middleware.uploadFileGCS.uploadImage,
+  consultationController.createConsultation
+);
+router.get(
+  "/:id",
+  middleware.authorization.authorize,
+  consultationController.getConsultationById
+);
+router.put(
+  "/:id",
+  middleware.authorization.authorize,
+  middleware.uploadFileGCS.uploadImage,
+  consultationController.updateConsultation
+);
 
-  module.exports = router
+module.exports = router;
